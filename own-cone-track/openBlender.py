13e46ce195067d6238729d639eb2cc9066630d9e
Raphael Schwinger <raphaelschwinger@gmail.com>
2021-07-07 14:58:21 +0200

    add openBlender docs

--- own-cone-track/openBlender.py
+++ own-cone-track/openBlender.py
@@ -1,3 +1,5 @@
+# open blender on mac os x with the path set to the current working directory
+# see https://stackoverflow.com/questions/9859404/opening-blender-a-program-from-a-specific-filepath-relative-paths-unix-execu/9940691#9940691
 import os
 import subprocess
 
