7a0eac2088e73c42b3c95fa323c9eeee4cf14b81
Raphael Schwinger <raphaelschwinger@gmail.com>
2021-07-27 13:58:14 +0000

    affine transformation

--- blender-car_animation/transform.py
+++ blender-car_animation/transform.py
@@ -3,12 +3,17 @@ import cv2
 import numpy as np
 import argparse
 
+
+"""
+Calculate the affine transformation matrix from 4 matching point pairs
+and apply the affine transformation matrix to all given points
+"""
 if __name__ == "__main__":
 
     # known 3d points
 
     known_points_3d = np.loadtxt('./known_points', delimiter=';')
-    print(known_points_3d)
+    print('known points: ', known_points_3d)
 
 	# load 3d points from file
     parser = argparse.ArgumentParser(description='Enter the 3d point file')
@@ -24,12 +29,30 @@ if __name__ == "__main__":
     
     points_3D = np.loadtxt(path, delimiter=';')
 
-    print(points_3D)
+    print('reconstructed points: ', points_3D)
 
     # getAffineTransformation
-    (retval, aff, inliers) = cv2.estimateAffine3D(points_3D[:3], known_points_3d)
-    print(retval)
-    print(aff)
-    print(inliers)
+    (retval, mat, out) = cv2.estimateAffine3D(points_3D[:4], known_points_3d)
+    print('affine matrix', mat)
+
+    # transform points
+
+    trans_points_3D = np.zeros(points_3D.shape, np.float32)
+
+    for i in range(len(points_3D)):
+        # add dimension
+        p_4d = np.zeros((4,1), np.float32)
+        p_4d[0] = points_3D[i][0]
+        p_4d[1] = points_3D[i][1]
+        p_4d[2] = points_3D[i][2]
+        p_4d[3] = 1.0
+        trans_point = np.dot(mat, p_4d)
+        print(trans_point)
+        # transform (3,1) to 3 and store in array
+        trans_points_3D[i] = trans_point.ravel()
+
+    print('Transformed point: ', trans_points_3D)
+
+  
 
     
