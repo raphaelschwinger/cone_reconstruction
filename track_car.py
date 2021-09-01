653a8da9e3eb180ff28b4facfcd3ee087ddc7558
Raphael Schwinger <raphaelschwinger@gmail.com>
2021-09-01 14:17:07 +0000

    WIP adapt track_car script

--- track_car.py
+++ track_car.py
@@ -2,6 +2,17 @@ import sys
 import cv2
  
 if __name__ == '__main__' :
+
+    parser = argparse.ArgumentParser(description='Enter the image folder')
+    parser.add_argument('dir_name', metavar='dir_name', type=str,
+                        help='Directory of images')
+
+    args = parser.parse_args()
+
+    directory_name = args.dir_name  # 'blender-house' or cone-track
+
+    path = os.path.abspath(os.path.join(
+        os.path.dirname(__file__), directory_name))
  
     tracker_types = ['KCF', 'CSRT']
     tracker_type = tracker_types[1]
@@ -13,7 +24,57 @@ if __name__ == '__main__' :
         tracker = cv2.TrackerCSRT_create()
 
     # Read video
-    video = cv2.VideoCapture("blender-car_animation/01-video0001-0080.avi")
+    video = cv2.VideoCapture("blender-racetrack/01-video0001-0080.avi")
+
+    # video = cv2.VideoCapture('./blender-racetrack/01-video0001-0080.avi')
+ 
+    # Exit if video not opened.
+    if not video.isOpened():
+        print("Could not open video")
+        sys.exit()
+ 
+    # Read first frame.
+    ok, frame = video.read()
+    if not ok:
+        print('Cannot read video file')
+        sys.exit()
+ 
+    # Define an initial bounding box
+    bbox = (287, 23, 86, 320)
+ 
+    # Uncomment the line below to select a different bounding box
+    bbox = cv2.selectROI(frame, False)
+ 
+    # Initialize tracker with first frame and bounding box
+    ok = tracker.init(frame, bbox)
+ 
+    while True:
+        # Read a new frame
+        ok, frame = video.read()
+        if not ok:
+            break
+         
+        # Start timer
+        timer = cv2.getTickCount()
+ 
+        # Update tracker
+        ok, bbox = tracker.update(frame)
+ 
+        # Calculate Frames per second (FPS)
+        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+ 
+        # Draw bounding box
+        if ok:
+            # Tracking success
+            p1 = (int(bbox[0]), int(bbox[1]))
+            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+            cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+        else :
+            # Tracking failure
+            cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+ 
+        # Display tracker type on frame
+        cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50
     # video = cv2.VideoCapture(0) # for using CAM
  
     # Exit if video not opened.
