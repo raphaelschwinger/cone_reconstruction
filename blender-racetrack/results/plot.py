f5068579b5def82a7d184c2a54acdab1c6b335d4
Raphael Schwinger <raphaelschwinger@gmail.com>
2021-09-09 09:25:48 +0200

    add cones to plot

--- blender-racetrack/results/plot.py
+++ blender-racetrack/results/plot.py
@@ -18,10 +18,20 @@ for result_path in results:
         # strip last dimension of points to make 2D points
         points_2D = points_3D[:, :2]
 
+        # dir_name
+        dir_name = os.path.basename(result_path)
+
         # plot points
         plt.plot(points_2D[:, 0], points_2D[:, 1],
-                 label=result_path.format(i=i))
+                 label=dir_name.format(i=i))
         plt.legend(loc='best')
+
+# add cone position to results
+blender_position_path = os.path.join(directory_name, 'blender-position')
+cone_points_3D = np.loadtxt(os.path.join(
+    blender_position_path, 'cone_transformation.p3d'), delimiter=';')
+cone_points_2D = cone_points_3D[:, :2]
+plt.plot(cone_points_2D[:, 0], cone_points_2D[:, 1], 'y^')
 
 
 plt.show()
