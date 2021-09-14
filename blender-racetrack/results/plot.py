324c8ee000402396d80f240868da54aa52eb2f34
Raphael Schwinger <raphaelschwinger@gmail.com>
2021-09-14 12:01:49 +0200

    calculate mean squard error

--- blender-racetrack/results/plot.py
+++ blender-racetrack/results/plot.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import os
 import numpy as np
 
+color = ['blue', 'green', 'orange']
+
 
 plt.figure(1)
 
@@ -13,19 +15,24 @@ results = sorted([os.path.join(directory_name, frame_dir)
 # load position data from blender
 blender_cone_path = os.path.join(directory_name, 'cone_position.p3d')
 blender_cone_points_3D = np.loadtxt(blender_cone_path, delimiter=';')
-plt.plot(blender_cone_points_3D[:, 0], blender_cone_points_3D[:, 1], 'r^')
 
 # load car position data from blender
 blender_car_path = os.path.join(directory_name, 'car_position.p3d')
 blender_car_points_3D = np.loadtxt(blender_car_path, delimiter=';')
-plt.plot(blender_car_points_3D[:, 0],
-         blender_car_points_3D[:, 1], label='real position'.format('r'))
+
+
+# add cone position to results
+blender_position_path = os.path.join(directory_name, 'blender-position')
+cone_points_3D = np.loadtxt(os.path.join(
+    blender_position_path, 'cone_transformation.p3d'), delimiter=';')
+transformed_cone_points_2D = cone_points_3D[:, :2]
+
 
 i = 0
 for result_path in results:
-    i += 1
     # check if result is a directory
     if os.path.isdir(result_path):
+        i += 1
         points_3D = np.loadtxt(os.path.join(
             result_path, 'car_transformation.p3d'), delimiter=';')
 
@@ -37,25 +44,29 @@ for result_path in results:
 
         # plot points
         plt.plot(points_2D[:, 0], points_2D[:, 1],
-                 label=dir_name.format(i=i))
+                 label=dir_name, color=color[i % 3])
         plt.legend(loc='best')
 
-# add cone position to results
-blender_position_path = os.path.join(directory_name, 'blender-position')
-cone_points_3D = np.loadtxt(os.path.join(
-    blender_position_path, 'cone_transformation.p3d'), delimiter=';')
-cone_points_2D = cone_points_3D[:, :2]
-plt.plot(cone_points_2D[:, 0], cone_points_2D[:, 1], 'y^')
+# add "real position" to plot
+plt.plot(blender_car_points_3D[:, 0],
+         blender_car_points_3D[:, 1], label='real position'.format('r'))
+plt.plot(blender_cone_points_3D[:, 0], blender_cone_points_3D[:, 1], 'r^')
+# add reconstucted cone points to plot
+plt.plot(transformed_cone_points_2D[:, 0],
+         transformed_cone_points_2D[:, 1], 'y^')
+plt.ylabel("Y position", labelpad=15)
+plt.xlabel("X position", labelpad=15)
 
 
 # "normalize points"
 
+# add cone position to results
 plt.figure(2)
 i = 0
 for result_path in results:
-    i += 1
     # check if result is a directory
     if os.path.isdir(result_path):
+        i += 1
         points_3D = np.loadtxt(os.path.join(
             result_path, 'car_transformation.p3d'), delimiter=';')
 
@@ -73,18 +84,42 @@ for result_path in results:
 
         # plot points
         plt.plot(points_2D[:, 0], points_2D[:, 1],
-                 label=dir_name.format(i=i))
+                 label=dir_name, color=color[i % 3])
         plt.legend(loc='best')
 
-# add cone position to results
-blender_position_path = os.path.join(directory_name, 'blender-position')
-cone_points_3D = np.loadtxt(os.path.join(
-    blender_position_path, 'cone_transformation.p3d'), delimiter=';')
-cone_points_2D = cone_points_3D[:, :2]
-plt.plot(cone_points_2D[:, 0], cone_points_2D[:, 1], 'y^')
-
-plt.plot(blender_cone_points_3D[:, 0], blender_cone_points_3D[:, 1], 'r^')
+# add "real position" to plot
 plt.plot(blender_car_points_3D[:, 0],
          blender_car_points_3D[:, 1], label='real position'.format('r'))
+plt.plot(blender_cone_points_3D[:, 0], blender_cone_points_3D[:, 1], 'r^')
+# add reconstucted cone points to plot
+plt.plot(transformed_cone_points_2D[:, 0],
+         transformed_cone_points_2D[:, 1], 'y^')
+plt.ylabel("Y position", labelpad=15)
+plt.xlabel("X position", labelpad=15)
+
+
+# Calculate MSE
+fig = plt.figure(3)
+
+
+i = 0
+for result_path in results:
+    # check if result is a directory
+    if os.path.isdir(result_path):
+        i += 1
+        points_3D = np.loadtxt(os.path.join(
+            result_path, 'car_transformation.p3d'), delimiter=';')
+
+        # calculate MSE
+        mse = np.mean(np.square(points_3D - blender_car_points_3D))
+        # dir_name
+        dir_name = os.path.basename(result_path)
+        print(dir_name, mse)
+        plt.text(i + 0.45, mse + 1, str(round(mse, 2)), fontweight='bold')
+        plt.bar(i, mse, 1, label=dir_name, color=color[i % 3])
+        plt.legend(loc='best')
+        plt.ylabel("Mean squared error", labelpad=15)
+        plt.xticks([])
+
 
 plt.show()
